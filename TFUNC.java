618d8e41ce45dca887a642413d4225d8974de347
maxrumbo <maxrumbo06@gmail.com>
2025-11-11 20:44:04 +0700

    del

--- TFUNC.java
+++ TFUNC.java
@@ -1,3 +1,37 @@
 // Nama - Nim
 // Nama - Nim
 // Nama - Nim (Optional)
+
+import java.util.Scanner;
+
+public class TFUNC {
+
+	public static int HitungRekursif(int angka) {
+		if (angka == 1) {
+			return 1;
+		}
+		return angka + HitungRekursif(angka - 1);
+	}
+
+	public static String buildSeries(int n) {
+		if (n == 1) {
+			return "1";
+		}
+		return buildSeries(n - 1) + "+" + n;
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		if (!sc.hasNextInt()) {
+			sc.close();
+			return;
+		}
+		int N = sc.nextInt();
+		int hasil = HitungRekursif(N);
+
+		System.out.println("Bilangan yang dimasukkan (N): " + N);
+		System.out.println("Hasil perhitungan rekursif (" + buildSeries(N) + "): " + hasil);
+
+		sc.close();
+	}
+}
